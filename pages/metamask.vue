060b265f04c7f4747cfaad0a66e1f086e6900830
hunter0409 <hunter12250409@gmail.com>
2021-11-09 11:40:28 +0300

    Coinlisting

---
+++ pages/metamask.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Metamask />
+  </div>
+</template>
+
+<script>
+import Metamask from '../components/Metamask'
+
+export default {
+  components: {
+    Metamask: Metamask
+}
+}
+</script>
+
+<style>
+
+</style>
